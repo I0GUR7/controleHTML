d0b34e6d50f938cbf0f4860ba00b5542f253290d
I0GUR7 <71790362+I0GUR7@users.noreply.github.com>
2021-08-12 09:24:00 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function mudaCory(el){
+    el.style.backgroundColor = 'yellow';
+    }
+
+    function mudaCorr(el){
+    el.style.backgroundColor = 'red';
+    }
+
+    function mudaCorb(el){
+    el.style.backgroundColor = 'blue';
+    }
+
+    function mudaCorg(el){
+    el.style.backgroundColor = 'green';
+    }
+
+    function mudaCor(el){
+    el.style.backgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+    }
